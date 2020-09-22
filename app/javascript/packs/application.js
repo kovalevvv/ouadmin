9950cba6801d36b0e4afcf7c15b40def33a2409e
Ковалев Владимир Викторович <kovalevvv3@it.mos.ru>
2020-09-22 12:32:29 +0300

    webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require('jquery')
+require("chart.js")
+require('bootstrap')
+require('datatables.net')
+require('datatables.net-bs4')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
