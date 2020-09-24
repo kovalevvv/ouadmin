db66f13aa95ed6803c27598431a9cd9fa4ba1df6
Ковалев Владимир Викторович <kovalevvv3@it.mos.ru>
2020-09-25 00:58:44 +0300

    css, tooltips

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("chart.js")
 require('bootstrap')
 require('datatables.net')
 require('datatables.net-bs4')
+require('jquery.tooltips')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
